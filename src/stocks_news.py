bbc6f8e22e68c83527297fcebfa69259de31de37
Koko-66 <abc@Kokos-Mac-mini.broadband>
2023-05-14 16:38:48 +0100

    Add filtering news by language.

--- src/stocks_news.py
+++ src/stocks_news.py
@@ -4,26 +4,39 @@ import json
 # import matplotlib.pyplot as plt
 import urllib.parse
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 # import yfinance as yf
 # import sqlite3
 
 
 stocks_news = Blueprint('stocks_news', __name__, url_prefix='/stocks/news')
 
-@stocks_news.route('/', methods=['GET'])
-def connect_to_api():
-    """Show all stocks"""
+@stocks_news.route('/', methods=['GET', 'POST'])
+def get_news():
+    """Get news about stocks"""
+    # Define the parameters of the web search.
+    # List of available languages: https://mediastack.com/documentation
+    languages = {"ar": "Arabic","de": "German", "en": "English", "es": "Spanish",
+                 "fr": "French", "he": "Hebrew", "it": "Italian", "nl": "Dutch", 
+                 "no": "Norwegian", "pt": "Portuguese", "ru": "Russian",
+                 "se": "Swedish", "zh": "Chinese"}
+    
+    #handle
+    if request.method == 'POST':
+        selected_language = request.form.get('language', 'en')
+
+    """Show news about stocks"""
     conn = http.client.HTTPConnection('api.mediastack.com')
     # Use keywords for web search as stock id related content.
     keywords = "google"
+
     # Define the parameters of the web search.
     params = urllib.parse.urlencode({
         'access_key': os.getenv('MEDIASTACK_API_KEY'),
-        'categories': 'general',
+        # 'categories': 'general',
         'sort': 'published_desc',
         'limit': 5,
-        'language': "en",
+        'languages': selected_language,
         'search': keywords,
         'keywords': keywords
     })
@@ -33,8 +46,10 @@ def connect_to_api():
     # parse the response data as JSON
     news_data = json.loads(news_data)
     # Extract the title and URL of the article.
-
-    articles = news_data['data']
+    try:
+        articles = news_data['data']
+    except KeyError:
+        articles = []
     # Print the title and URL.
 
     conn.close()
@@ -42,5 +57,9 @@ def connect_to_api():
     # add handling same articles from different sources - so that they do not repeat!
     
     
-    return render_template('stocks_news.html', news_data=news_data, articles=articles)
+    return render_template('stocks_news.html', 
+                           news_data=news_data, 
+                           articles=articles,
+                           languages=languages,
+                           selected_language=selected_language)
 
