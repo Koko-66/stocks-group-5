109ea9c4aae3fab4294dac93e1bba333940f541f
Koko <pointmisser@gmail.com>
2023-05-14 22:13:03 +0100

    Update branch with changes from main.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for
 import os
 from src.auth import auth
 from src.database import db
@@ -14,7 +14,8 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI")
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
             )
     # if test_config is not None, then the app is in test mode
     else:
