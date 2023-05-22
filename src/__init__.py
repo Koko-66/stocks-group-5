eb06386132d49ac4ad0933bbfeeba0011eb84e38
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2023-05-22 22:07:02 +0100

    Revert __init__.py to previous commit

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, redirect, session, url_for
 import os
 from src.auth import auth
 from src.database import db
-from flask_session import Session
-from src.auth import login_required
-
 
 
 def create_app(test_config=None):
@@ -18,11 +15,8 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
-            SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            SESSION_TYPE='filesystem',
-            SESSION_PERMANENT=True,
-            SESSION_USE_SIGNER=True
-        )
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
+            )
     # if test_config is not None, then the app is in test mode
     else:
         app.config.from_object(test_config)
@@ -32,12 +26,10 @@ def create_app(test_config=None):
     db.app=app
     db.init_app(app)
 
-    Session(app)
-
 
     #registering blueprints
 
-    blueprints = ['auth', 'home', 'stocks_news', 'stocks_graph', 'profile', 'notifications']
+    blueprints = ['auth', 'home', 'stocks_news', 'profile', 'stocks_graph', 'notifications']
     
     for bp in blueprints:
         app.register_blueprint(__import__(f'src.{bp}', fromlist=[bp]).__getattribute__(bp))
